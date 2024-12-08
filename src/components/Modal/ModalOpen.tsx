3b4be8ca043f2fdc373fda56c168f8eb27cdc24a
Alexander Vazquez <avazquez@debt.com>
2024-12-08 16:44:16 -0500

    wrapped modal with a portal and added some comments about missing focus management,time is over

--- src/components/Modal/ModalOpen.tsx
+++ src/components/Modal/ModalOpen.tsx
@@ -1,15 +1,24 @@
 import ModalProps from './Modal.types';
 import {useOnKeyDown, useOnClickOutside} from '../../hooks';
 import { useRef } from 'react';
+import {createPortal} from 'react-dom';
+
+
+//Important things that are missing here:
+// The Modal is missing proper focus management and aria-modal="true" for that reason, which is critical for the user experience, 
+// especially for users relying on assistive technologies. Without focus management, navigation becomes disruptive and inaccessible, 
+// negatively impacting usability for all users 
 
 export default function ModalOpen({title,children,onClose,"data-testid":testId}:Omit<ModalProps,"isOpen"> ) {
     const modalRef = useRef<HTMLDivElement>(null);
-    
+    // I created these custom hooks to ensure that this logic could be easily reused in other components in the future.
     useOnKeyDown("Escape", onClose);
     useOnClickOutside(modalRef, onClose);
 
     return (
-        <div className='modal-backdrop' data-testid={testId}>
+        // Wrapped the modal in a portal to ensure it renders correctly without interference from its parent styles, 
+        // as it will be appended at the end of the body.
+        createPortal(<div className='modal-backdrop' data-testid={testId}>
             <div className='modal-dialog' role='dialog' ref={modalRef}>
                 <div className='modal-header'>
                     {/* h2 tags get heading role automatically */}
@@ -20,6 +29,6 @@ export default function ModalOpen({title,children,onClose,"data-testid":testId}:
                     <button type="button" aria-label="Close modal" onClick={onClose}>Close</button>
                 </div>
             </div>
-        </div>
+        </div>, document.body)
     )
 }
