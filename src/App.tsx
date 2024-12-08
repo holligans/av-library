8f8521a87f0cdd724b952f536693a2740660167e
Alexander Vazquez <avazquez@debt.com>
2024-12-08 15:00:39 -0500

    Modal basic logic implemented, basic styling applied too

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { Modal } from './components/Modal';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, toggleModal] = useState(false);
+
+  const onClose = () => {
+    toggleModal(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => toggleModal(true)}>
+          Show Modal
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Modal title="Testing Title" isOpen={isOpen} onClose={onClose}>
+          <p>Testing modal body</p>
+        </Modal>  
     </>
   )
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,17 +1,22 @@
 import ModalProps from './Modal.types';
+import "./Modal.styles.css"
 
-export default function Modal(props:ModalProps) {
-  return (
-    <div className='modal-backdrop'>
-        <div className='modal-dialog'>
-            <div className='modal-header'>
-                <h2>{props.title}</h2>
-            </div>
-            <div className='modal-body'>{props.children}</div>
-            <div className='modal-footer'>
-                <button type="button">Close</button>
+
+export default function Modal({isOpen, title, children, onClose}:ModalProps) {
+    if(!isOpen){
+        return null;
+    }
+    return (
+        <div className='modal-backdrop'>
+            <div className='modal-dialog'>
+                <div className='modal-header'>
+                    <h2>{title}</h2>
+                </div>
+                <div className='modal-body'>{children}</div>
+                <div className='modal-footer'>
+                    <button type="button" onClick={onClose}>Close</button>
+                </div>
             </div>
         </div>
-    </div>
   )
 }
