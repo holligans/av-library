bfc05820fdb4b7c74c7b56fa1195b23a0d2f69ed
Alexander Vazquez <avazquez@debt.com>
2024-12-08 15:55:22 -0500

    testing configured and running fine

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], //different Vite versions in the project causing this issue
-  test:{
-    environment:"jsdom",
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
     globals:true,
   }
 })
+
