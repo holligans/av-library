7ae55f4802f3790684cb880f1bcdab8930c805a9
Alexander Vazquez <avazquez@debt.com>
2024-12-08 14:37:52 -0500

    testing tool installed,problem when configuring in vite.config it seems to be produced by having different vite versions in the project dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], //different Vite versions in the project causing this issue
+  test:{
+    environment:"jsdom",
+    globals:true,
+  }
 })
